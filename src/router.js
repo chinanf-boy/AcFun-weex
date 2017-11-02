2a88f9f917381440da0b55bbbb70bd29918f456a
chinanf-boy <865501259@qq.com>
2017-11-02 16:03:20 +0800

    change router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 // import Vue from 'vue'
 import Router from 'vue-router'
 import HomeView from "./views/HomeView.vue"
-import ActicleView from "./views/ActivleView.vue"
+import ActicleView from "./views/ActicleView.vue"
 Vue.use(Router)
 
 // Story view factory
