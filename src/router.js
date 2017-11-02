962f1324829fc7abd903e8f11e6203adc6129e59
chinanf-boy <865501259@qq.com>
2017-11-02 16:03:35 +0800

    change router

--- src/router.js
+++ src/router.js
@@ -4,15 +4,15 @@ import HomeView from "./views/HomeView.vue"
 import ActicleView from "./views/ActicleView.vue"
 Vue.use(Router)
 
-// Story view factory
-function createStoriesView (type) {
-  return {
-    name: `${type}-stories-view`,
-    render (createElement) {
-      return createElement(StoriesView, { props: { type }})
-    }
-  }
-}
+// // Story view factory
+// function createStoriesView (type) {
+//   return {
+//     name: `${type}-stories-view`,
+//     render (createElement) {
+//       return createElement(StoriesView, { props: { type }})
+//     }
+//   }
+// }
 
 export default new Router({
   // mode: 'abstract',
